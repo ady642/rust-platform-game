75f00aae5bf42fe54347fca4deefb4662acfb02a
Adrien Haik <ady642@gmail.com>
2023-08-25 17:57:45 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,103 @@
+use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
+use bevy::window::WindowResolution;
+use bevy_rapier2d::prelude::*;
+
+const WINDOW_WIDTH: f32 = 1024.0;
+const WINDOW_HEIGHT: f32 = 720.0;
+
+const WINDOW_BOTTOM_Y: f32 = -WINDOW_HEIGHT / 2.0;
+const WINDOW_LEFT_X: f32 = -WINDOW_WIDTH / 2.0;
+
+const COLOR_BACKGROUND: Color = Color::rgb(0.29, 0.31, 0.41);
+const COLOR_PLATFORM: Color = Color::rgb(0.13, 0.13, 0.23);
+const COLOR_PLAYER: Color = Color::rgb(0.60, 0.55, 0.60);
+
+const FLOOR_THICKNESS: f32 = 10.0;
+
+const COLOR_FLOOR: Color = Color::rgb(0.45, 0.55, 0.66);
+
+#[derive(Bundle)]
+struct PlatformBundle {
+    sprite_bundle: SpriteBundle,
+    body: RigidBody,
+    collider: Collider,
+}
+
+impl PlatformBundle {
+    fn new(x: f32, scale: Vec3) -> Self {
+        Self {
+            sprite_bundle: SpriteBundle {
+                sprite: Sprite {
+                    color: COLOR_PLATFORM,
+                    ..Default::default()
+                },
+                transform: Transform {
+                    translation: Vec3::new(x, WINDOW_BOTTOM_Y + (scale.y / 2.0), 0.0),
+                    scale,
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            body: RigidBody::Fixed,
+            collider: Collider::cuboid(0.5, 0.5),
+        }
+    }
+}
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(COLOR_BACKGROUND)) // resource added
+        .add_plugins((DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Bevy Platformer".to_string(),
+                resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+                resizable: false,
+                ..Default::default()
+            }),
+            ..Default::default()
+        }),
+        RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(200.0),
+        RapierDebugRenderPlugin::default()
+        ))
+        .add_systems(Startup,setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>
+) {
+    commands.spawn(PlatformBundle::new(-100.0, Vec3::new(75.0, 200.0, 1.0)));
+    commands.spawn(PlatformBundle::new(100.0, Vec3::new(50.0, 350.0, 1.0)));
+    commands.spawn(PlatformBundle::new(350.0, Vec3::new(150.0, 250.0, 1.0)));
+
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: COLOR_FLOOR,
+                ..Default::default()
+            },
+            transform: Transform {
+                translation: Vec3::new(0.0, WINDOW_BOTTOM_Y  + (FLOOR_THICKNESS / 2.0), 0.0),
+                scale: Vec3::new(WINDOW_WIDTH, FLOOR_THICKNESS, 1.0),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(RigidBody::Fixed)
+        .insert(Collider::cuboid(0.5, 0.5));
+
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::Circle::default().into()).into(),
+        material: materials.add(ColorMaterial::from(COLOR_PLAYER)),
+        transform: Transform {
+            translation: Vec3::new(WINDOW_LEFT_X + 100.0, WINDOW_BOTTOM_Y + 30.0, 0.0),
+            scale: Vec3::new(30.0, 30.0, 1.0),
+            ..Default::default()
+        },
+        ..default()
+    });
+    commands.spawn(Camera2dBundle::default());
+}
