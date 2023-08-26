d3e3ad8e22a07d670a30804a4f051be7e246210a
Adrien Haik <ady642@gmail.com>
2023-08-26 12:44:45 +0200

    add plugin and movment

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod platforms;
+
+use platforms::PlatformsPlugin;
+
 use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
 use bevy::window::WindowResolution;
@@ -10,40 +14,20 @@ const WINDOW_BOTTOM_Y: f32 = -WINDOW_HEIGHT / 2.0;
 const WINDOW_LEFT_X: f32 = -WINDOW_WIDTH / 2.0;
 
 const COLOR_BACKGROUND: Color = Color::rgb(0.29, 0.31, 0.41);
-const COLOR_PLATFORM: Color = Color::rgb(0.13, 0.13, 0.23);
 const COLOR_PLAYER: Color = Color::rgb(0.60, 0.55, 0.60);
 
 const FLOOR_THICKNESS: f32 = 10.0;
 
 const COLOR_FLOOR: Color = Color::rgb(0.45, 0.55, 0.66);
 
-#[derive(Bundle)]
-struct PlatformBundle {
-    sprite_bundle: SpriteBundle,
-    body: RigidBody,
-    collider: Collider,
-}
+const PLAYER_VELOCITY_X: f32 = 400.0;
 
-impl PlatformBundle {
-    fn new(x: f32, scale: Vec3) -> Self {
-        Self {
-            sprite_bundle: SpriteBundle {
-                sprite: Sprite {
-                    color: COLOR_PLATFORM,
-                    ..Default::default()
-                },
-                transform: Transform {
-                    translation: Vec3::new(x, WINDOW_BOTTOM_Y + (scale.y / 2.0), 0.0),
-                    scale,
-                    ..Default::default()
-                },
-                ..Default::default()
-            },
-            body: RigidBody::Fixed,
-            collider: Collider::cuboid(0.5, 0.5),
-        }
-    }
-}
+const PLAYER_VELOCITY_Y: f32 = 850.0;
+
+const MAX_JUMP_HEIGHT: f32 = 230.0;
+
+#[derive(Component)]
+struct Jump(f32);
 
 fn main() {
     App::new()
@@ -58,9 +42,11 @@ fn main() {
             ..Default::default()
         }),
         RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(200.0),
-        RapierDebugRenderPlugin::default()
+        RapierDebugRenderPlugin::default(),
+        PlatformsPlugin
         ))
         .add_systems(Startup,setup)
+        .add_systems(Update,(movement, jump, rise, fall)) // new system added
         .run();
 }
 
@@ -69,10 +55,6 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>
 ) {
-    commands.spawn(PlatformBundle::new(-100.0, Vec3::new(75.0, 200.0, 1.0)));
-    commands.spawn(PlatformBundle::new(100.0, Vec3::new(50.0, 350.0, 1.0)));
-    commands.spawn(PlatformBundle::new(350.0, Vec3::new(150.0, 250.0, 1.0)));
-
     commands
         .spawn(SpriteBundle {
             sprite: Sprite {
@@ -98,6 +80,93 @@ fn setup(
             ..Default::default()
         },
         ..default()
-    });
+    })
+    .insert(RigidBody::KinematicPositionBased)
+    .insert(Collider::ball(0.5))
+    .insert(KinematicCharacterController::default());
+
     commands.spawn(Camera2dBundle::default());
+}
+
+fn movement(
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+    mut query: Query<&mut KinematicCharacterController>,
+) {
+    let mut player = query.single_mut();
+
+    let mut movement = 0.0;
+
+    if input.pressed(KeyCode::Right) {
+        movement += time.delta_seconds() * PLAYER_VELOCITY_X;
+    }
+
+    if input.pressed(KeyCode::Left) {
+        movement += time.delta_seconds() * PLAYER_VELOCITY_X * -1.0;
+    }
+
+    match player.translation {
+        Some(vec) => player.translation = Some(Vec2::new(movement, vec.y)), // update if it already exists
+        None => player.translation = Some(Vec2::new(movement, 0.0)),
+    }
+}
+
+fn jump(
+    input: Res<Input<KeyCode>>,
+    mut commands: Commands,
+    query: Query<
+        (Entity, &KinematicCharacterControllerOutput),
+        (With<KinematicCharacterController>, Without<Jump>),
+    >,
+) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (player, output) = query.single();
+
+    if input.pressed(KeyCode::Up) && output.grounded {
+        commands.entity(player).insert(Jump(0.0));
+    }
+}
+fn rise(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut query: Query<(Entity, &mut KinematicCharacterController, &mut Jump)>,
+) {
+    if query.is_empty() {
+        return;
+    }
+
+    let (entity, mut player, mut jump) = query.single_mut();
+
+    let mut movement = time.delta().as_secs_f32() * PLAYER_VELOCITY_Y;
+
+    if movement + jump.0 >= MAX_JUMP_HEIGHT {
+        movement = MAX_JUMP_HEIGHT - jump.0;
+        commands.entity(entity).remove::<Jump>();
+    }
+
+    jump.0 += movement;
+
+    match player.translation {
+        Some(vec) => player.translation = Some(Vec2::new(vec.x, movement)),
+        None => player.translation = Some(Vec2::new(0.0, movement)),
+    }
+}
+
+fn fall(time: Res<Time>, mut query: Query<&mut KinematicCharacterController, Without<Jump>>) {
+    if query.is_empty() {
+        return;
+    }
+
+    let mut player = query.single_mut();
+
+    // I am using two-thirds of the Y-velocity since I want the character to fall slower than it rises
+    let movement = time.delta().as_secs_f32() * (PLAYER_VELOCITY_Y / 1.5) * -1.0;
+
+    match player.translation {
+        Some(vec) => player.translation = Some(Vec2::new(vec.x, movement)),
+        None => player.translation = Some(Vec2::new(0.0, movement)),
+    }
 }
---
+++ src/platforms.rs
@@ -0,0 +1,46 @@
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+
+const COLOR_PLATFORM: Color = Color::rgb(0.13, 0.13, 0.23);
+
+#[derive(Bundle)]
+struct PlatformBundle {
+    sprite_bundle: SpriteBundle,
+    body: RigidBody,
+    collider: Collider,
+}
+
+impl PlatformBundle {
+    fn new(x: f32, scale: Vec3) -> Self {
+        Self {
+            sprite_bundle: SpriteBundle {
+                sprite: Sprite {
+                    color: COLOR_PLATFORM,
+                    ..Default::default()
+                },
+                transform: Transform {
+                    translation: Vec3::new(x, 0.0, 0.0),
+                    scale,
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            body: RigidBody::Dynamic,
+            collider: Collider::cuboid(0.5, 0.5),
+        }
+    }
+}
+
+pub struct PlatformsPlugin;
+
+impl Plugin for PlatformsPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup);
+    }
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(PlatformBundle::new(-100.0, Vec3::new(75.0, 200.0, 1.0)));
+    commands.spawn(PlatformBundle::new(100.0, Vec3::new(50.0, 350.0, 1.0)));
+    commands.spawn(PlatformBundle::new(350.0, Vec3::new(150.0, 250.0, 1.0)));
+}
